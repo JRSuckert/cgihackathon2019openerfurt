97da7e6129d720c1fb125aaa99d2adf50e1e14ad
Marcel Gohsen <marcel.gohsen@uni-weimar.de>
2018-12-01 14:06:14 +0100

    Wrapper: Specification parsing

--- src_py/db_wrapper/db_wrapper.py
+++ src_py/db_wrapper/db_wrapper.py
@@ -11,17 +11,19 @@ def connect_to_db(user, password):
         port="9006"
     )
 
+def create_table(connection, spec_json):
+    pass
+
 
 def main(args):
-    connection = connect_to_db(args.user, args.password)
-
+    connection = connect_to_db(args.u, args.p)
     connection.close()
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("user", metavar="user", type=str)
-    parser.add_argument("password", metavar="password", type=str)
+    parser.add_argument("-u", metavar="user", type=str)
+    parser.add_argument("-p", metavar="password", type=str)
 
     args = parser.parse_args()
     main(args)
